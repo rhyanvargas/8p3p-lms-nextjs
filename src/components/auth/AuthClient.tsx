de2a557fdd15821895e1ef08af76a3e64b9fdfc3
Rhyguy <rhyan@rhyanvargas.com>
2025-09-06 11:20:22 -0700

    fix: update Amplify outputs import path to use alias syntax

--- src/components/auth/AuthClient.tsx
+++ src/components/auth/AuthClient.tsx
@@ -2,7 +2,7 @@
 
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
-import outputs from "../../../amplify_outputs.json";
+import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
